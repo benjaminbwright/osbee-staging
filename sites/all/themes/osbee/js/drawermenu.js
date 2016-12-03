ff8f94c1b8b2ff3d31836229dde47c382edec3c6
Benjamin Wright <benjamin@davenporttheatrical.com>
2016-12-02 23:58:37 -0500

    Added logo and copy to vertical slidshow and debugged sizes on various screens.

--- sites/all/themes/osbee/js/drawermenu.js
+++ sites/all/themes/osbee/js/drawermenu.js
@@ -3,26 +3,27 @@
         //Drawer Menu
         menuToggle = $(".toggle-menu");
         drawer = $("#drawer-menu");
-        page = $(".page-node");
+        page = $("#page");
         overlay = $('#drawer-menu-overlay');
         
+        drawer.toggleClass("off-screen")
         overlay.hide();
         $(".toggle-menu .toggle-close").hide();
 
         menuToggle.click(function(){
-            if (page.hasClass("off-screen")) {
+            if (drawer.hasClass("off-screen")) {
                 setTimeout(function(){drawer.toggleClass("on-screen")},100);
-                overlay.hide();
-                $(".toggle-open", this).show();
-                $(".toggle-close", this).hide();
-
-            } else {
-                drawer.toggleClass("on-screen");
                 overlay.show();
                 $(".toggle-open", this).hide();
                 $(".toggle-close", this).show();
+
+            } else {
+                drawer.toggleClass("on-screen");
+                overlay.hide();
+                $(".toggle-open", this).show();
+                $(".toggle-close", this).hide();
             }
-            page.toggleClass("off-screen");
+            drawer.toggleClass("off-screen");
         });
     });
 })(jQuery);
