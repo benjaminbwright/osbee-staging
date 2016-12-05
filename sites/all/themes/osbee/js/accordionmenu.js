92da981debbc7f068e1be87c3a53421529b4a98b
Benjamin Wright <benjamin@davenporttheatrical.com>
2016-12-05 00:37:51 -0500

    Setup active menu item functionality.

--- sites/all/themes/osbee/js/accordionmenu.js
+++ sites/all/themes/osbee/js/accordionmenu.js
@@ -2,9 +2,18 @@
     $(document).ready(function(){
         $('#mobile-navigation ul.menu li ul').addClass('menu-subcat');
         $('#mobile-navigation .menu-subcat').prev('a').after('<span class="fa subcat-expand">&#x33;</span>');
+        $('#mobile-navigation li.active-trail .subcat-expand').html("&#x32;");
+        $('#mobile-navigation li.active-trail:first').toggleClass('active');
+        $('#mobile-navigation li.active-trail a.active').parent().toggleClass('active');
         $('#mobile-navigation .subcat-expand').click(function(){
-            $(this).parent().toggleClass('active-menu-item');
-            if ($(this).parent().hasClass('active-menu-item')) {
+            $("#mobile-navigation li").each(function(){
+                if ($(this).hasClass('active')) {
+                    $(this).removeClass('active');
+                    $(this).find(".subcat-expand").html("&#x33;");
+                }
+            });
+            $(this).parent().toggleClass('active');
+            if ($(this).parent().hasClass('active')) {
                 $(this).html("&#x32;");
             } else {
                 $(this).html("&#x33;");
