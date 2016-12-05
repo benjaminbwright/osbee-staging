2c6494a6436882c7ab68bd6d6ca684a00f56d7ea
Benjamin Wright <benjamin@davenporttheatrical.com>
2016-12-05 13:31:48 -0500

    Added locations to footer contact info.

--- sites/all/themes/osbee/js/accordionmenu.js
+++ sites/all/themes/osbee/js/accordionmenu.js
@@ -1,20 +1,30 @@
 (function($){
     $(document).ready(function(){
+        // Add the menu-subcat class to the mobile menu elements with children
         $('#mobile-navigation ul.menu li ul').addClass('menu-subcat');
+        // Add the subcategory toggle element
         $('#mobile-navigation .menu-subcat').prev('a').after('<span class="fa subcat-expand">&#x33;</span>');
+        // Change the toggle element icon for active elements - if a child page is currently open
         $('#mobile-navigation li.active-trail .subcat-expand').html("&#x32;");
+        // Only toggle the highlighting on the first element in an active trail
         $('#mobile-navigation li.active-trail:first').toggleClass('active');
+        // Make the parent li of an active link active
         $('#mobile-navigation li.active-trail a.active').parent().toggleClass('active');
+        
         $('#mobile-navigation .subcat-expand').click(function(){
+            // Open/Close Menu Itemw
             if (!$(this).parent().hasClass('active')) {
+                // Close any open menu items if clicking a closed menu item
                 $("#mobile-navigation li").each(function(){
                     if ($(this).hasClass('active')) {
                         $(this).removeClass('active');
                         $(this).find(".subcat-expand").html("&#x33;");
                     }
                 });
-                $(this).parent().addClass('active');
+                // Open the new menu item
+                $(this).parent().addClass('active', 400, 'easeOutQuart');
             } else {
+                // Only close the current menu item
                 $(this).parent().removeClass('active');
             }
 
