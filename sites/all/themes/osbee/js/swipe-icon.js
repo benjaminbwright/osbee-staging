c8f160c0ecfca5172abc484a35934862d2374257
Benjamin Wright <benjamin@davenporttheatrical.com>
2016-12-05 17:05:56 -0500

    Stopped swiper icon from showing up on pages without slideshows.

--- sites/all/themes/osbee/js/swipe-icon.js
+++ sites/all/themes/osbee/js/swipe-icon.js
@@ -1,8 +1,17 @@
 (function($){
     $(document).ready(function(){
-        $("body").prepend('<div class="swipe-icon-overlay"><img class="swipe-icon" src="sites/all/themes/osbee/images/swipe.png" /></div>');
+        
+        if ($(".views_slideshow_main").length > 0) {
+            $("body").prepend('<div class="swipe-icon-overlay"><img class="swipe-icon" src="/sites/all/themes/osbee/images/swipe.png" /></div>');
+        }
+        //$(".swipe-icon-overlay").detach().prependTo("body");
         //$(".swipe-icon-overlay").fadeOut(12000);
+
+        // Hide the icon if clicked or swiped
         $(".swipe-icon-overlay").click(function(){
+            $(this).css("display", "none");
+        });
+        $(".swipe-icon-overlay").bind('touchstart mousedown', function(e){
             $(this).css("display", "none");
         });
     });
