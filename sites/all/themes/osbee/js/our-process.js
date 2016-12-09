3b14161bc17e338d5a10ac9ff55fe6f048a43515
Benjamin Wright <benjamin@davenporttheatrical.com>
2016-12-09 14:56:08 -0500

    Styled Our Process section.

---
+++ sites/all/themes/osbee/js/our-process.js
@@ -0,0 +1,56 @@
+/////////////////////
+//Our Process Menu //
+/////////////////////
+
+(function($){
+    $(document).ready(function(){
+        // Move main content on main services page
+        
+        //$(".menu-block-3 .active-trail .menu-link-wrapper").first().after('<ul class="menu menu-subcat"><li><div id="active-content" class="responsive-narrow-body"></div></li></ul>');
+       /* $(".menu-block-3 li .menu-link-wrapper").after('<span class="fa subcat-expand">&#x33;</span>');
+        $(".menu-block-3 .active-trail .subcat-expand").html("&#x32;");
+
+
+        var pageContent = $(".under-main-image");
+        //var pageImage = $(".under-main-image");
+        var responsiveContent = $(".responsive-narrow-body").html();
+
+        pageContent.clone().prependTo('.menu-block-3 .responsive-narrow-body');
+        //pageImage.clone().prependTo('.responsive-narrow-body');
+
+        // Toggle between responsive and desktop content
+        if ($(window).width() < 769 ){
+            pageContent.hide();
+            //pageImage.hide();
+            responsiveContent.show();
+        } else {
+            pageContent.show();
+            //pageImage.show();
+            responsiveContent.hide();
+        }
+        
+        $(window).resize(function(){
+            if ($(window).width() < 769 ){
+                pageContent.hide();
+                //pageImage.hide();
+                responsiveContent.show();
+            } else {
+                pageContent.show();
+                //pageImage.show();
+                responsiveContent.hide();
+            }
+        });
+
+        // Scroll to active menu item
+        var container = $("html,body");
+        var scrollTo = $(".menu-block-3 .active-trail:visible:first");
+        //var scrollTo = $("#node-page-full-group-prev-next");
+
+        if (scrollTo.offset() !== null) { 
+            container.animate({ scrollTop: scrollTo.offset().top},'1200');
+        }
+
+        */
+
+    });
+})(jQuery);
