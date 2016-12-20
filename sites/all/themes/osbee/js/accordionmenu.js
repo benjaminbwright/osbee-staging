dd7bccdc25fe4e9991d4e3384d487c58e2351a69
Benjamin Wright <benjamin@davenporttheatrical.com>
2016-12-20 13:39:06 -0500

    Updates to mobile nav functionality.

--- sites/all/themes/osbee/js/accordionmenu.js
+++ sites/all/themes/osbee/js/accordionmenu.js
@@ -1,36 +1,51 @@
 (function($){
     $(document).ready(function(){
+        
+        ///////////////////////////
+        // Mobile Accordion Menu //
+        ///////////////////////////
+
         // Add the menu-subcat class to the mobile menu elements with children
         $('#mobile-navigation ul.menu li ul').addClass('menu-subcat');
         // Add the subcategory toggle element
         $('#mobile-navigation .menu-subcat').prev('a').after('<span class="fa subcat-expand">&#x33;</span>');
         // Change the toggle element icon for active elements - if a child page is currently open
-        $('#mobile-navigation li.active-trail .subcat-expand').html("&#x32;");
+        $('#mobile-navigation li.active-trail > .subcat-expand').html("&#x32;");
         // Only toggle the highlighting on the first element in an active trail
         $('#mobile-navigation li.active-trail:first').toggleClass('active');
         // Make the parent li of an active link active
-        $('#mobile-navigation li.active-trail a.active').parent().toggleClass('active');
-        
+        $('#mobile-navigation li.active-trail a.active').parent().addClass('active');
+        // Highlight the first active-trail item.
         $("#mobile-navigation li.active-trail").first().addClass("first-active");
 
         $('#mobile-navigation .subcat-expand').click(function(){
-            // Open/Close Menu Itemw
+            // Open/Close Menu Item
             if (!$(this).parent().hasClass('active')) {
                 // Close any open menu items if clicking a closed menu item
-                $("#mobile-navigation li").each(function(){
+               $(this).parent().parent().find("li").each(function(){
                     if ($(this).hasClass('active')) {
                         $(this).removeClass('active');
                         $(this).find(".subcat-expand").html("&#x33;");
                     }
                 });
-        $("#mobile-navigation .first-active").removeClass("active-trail");
-
+                $("#mobile-navigation .first-active").removeClass("active-trail");
+                
                 // Open the new menu item
-                $(this).parent().addClass('active', 400, 'easeOutQuart');
+                $(this).parent().addClass('active');
+
+                if ($(this).parent().hasClass('active-trail')) {
+                    $(this).parent().removeClass('active').removeClass('active-trail').addClass('persistant-trail');
+                }
+                
+                $(this).find('a.active-trail').parent().addClass('active-trail');
+
+
             } else {
                 // Only close the current menu item
                 $(this).parent().removeClass('active');
             }
+
+            $('#mobile-navigation a.active').parent().addClass('active');
 
             if ($(this).parent().hasClass('active')) {
                 $(this).html("&#x32;");
@@ -39,7 +54,11 @@
             }
         });
 
-        // INPAGE ACCORDION MENU
+
+
+        ///////////////////////////
+        // INPAGE ACCORDION MENU //
+        ///////////////////////////
 
         $('#main-content ul.menu li ul').addClass('menu-subcat');
         $('#main-content .menu-subcat').prev('a').after('<span class="fa subcat-expand">&#x33;</span>');
@@ -53,7 +72,12 @@
         });
 
 
-        // TEAM PAGE ACCORDION MENU
+
+
+        //////////////////////////////
+        // TEAM PAGE ACCORDION MENU //
+        //////////////////////////////
+        
         $('.section-about-us .profile').each(function(){
             $(".profile__image", this).before($(".profile__header-links", this));
         });
@@ -88,6 +112,8 @@
             }
 
         });
+
+
 
         // Toggle Individual profiles =================
         // Add expanded class to all of the 1st profiles in each profile group.
@@ -129,22 +155,6 @@
         });
 
         $(".section-about-us .subcat-expand").eq(0).trigger('click');
-
-/*        // CAREERS PAGE ACCORDION MENU
-        
-        // Add the menu-subcat class to the mobile menu elements with children
-        $('.page-careers ul.menu li ul').addClass('menu-subcat');
-        // Add the subcategory toggle element
-        $('.page-careers .menu-subcat').prev('a').after('<span class="fa subcat-expand">&#x33;</span>');
-        // Change the toggle element icon for active elements - if a child page is currently open
-        $('.page-careers li.active-trail .subcat-expand').html("&#x32;");
-        // Only toggle the highlighting on the first element in an active trail
-        $('.page-careers li.active-trail:first').toggleClass('active');
-        // Make the parent li of an active link active
-        $('.page-careers li.active-trail a.active').parent().toggleClass('active');
-        
-        $(".page-careers li.active-trail").first().addClass("first-active");
-        */
 
     });
 })(jQuery);
